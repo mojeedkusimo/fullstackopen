5f8adf951a0f26af68f7bd21b3847297f72c1bca
mojeedkusimo <mkusimo90@gmail.com>
2023-07-13 05:06:52 +0100

    Complete Exercise: 3.7

---
+++ part_3/3-7/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+app.use(express.json());
+app.use(morgan('tiny'));
+
+let phoneBookEntries = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+const generateID = () => {
+
+  let newID = Math.round(Math.random() * 5000);
+  return newID;
+}
+
+const checkDuplicate = (name) => {
+  const nameArray = phoneBookEntries.map(entry => entry.name.toLowerCase());
+
+  return nameArray.includes(name.toLowerCase());
+}
+
+app.get('/api/persons', (req, res) => {
+  res.json(phoneBookEntries);
+});
+
+
+app.post('/api/persons', (req, res) => {
+  const {name, number} = req.body;
+
+  if (!name || !number) {
+    return res.status(400).json({
+      error: "Name or number is missing!"
+    });
+  } else if (checkDuplicate(name)) {
+    return res.status(400).json({
+      error: "Name already exists!"
+    });
+  }
+
+  const id = generateID();
+
+  const newEntry = {
+    id,
+    name,
+    number
+  };
+
+  phoneBookEntries = phoneBookEntries.concat(newEntry);
+
+  res.json(phoneBookEntries);
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001...');
+});
